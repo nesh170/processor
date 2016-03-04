5114e5dbb2688ff4f8a0f3bf88d7c3cdf53994a1
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-03-03 23:03:44 -0500

    testing mult module

--- multdiv/mult_module.v
+++ multdiv/mult_module.v
@@ -4,12 +4,20 @@ module mult_module(data_operandA, data_operandB, clock, ctrl_MULT, data_result, 
    input clock, ctrl_MULT;      
    output [31:0] data_result; 
    output data_exception, data_inputRDY, data_resultRDY; 
+	
+	wire exception_wire[1:0];
+	assign data_exception = exception_wire[1] | exception_wire[0];
+	assign exception_wire[0] = 0;
+	
+	//xor xor_gate(exception_wire[0],data_operandA[31:16]);
 
 	/*
 	Handling Reset Cases
 	*/
 	wire reset_wire; 
-	assign reset_wire	= data_exception | ~ctrl_MULT;
+	assign reset_wire	= exception_wire[1] | exception_wire[0] | ~ctrl_MULT;
+	
+	
 	
 	/*
 		Handling the D-Flip-Flop Operation
@@ -38,6 +46,6 @@ module mult_module(data_operandA, data_operandB, clock, ctrl_MULT, data_result, 
 	wire carry_in;
 	wire[31:0] booth_output;
 	booth_module booth_operations(data_operandA,data_operandB, counter_output, booth_output, carry_in); //this does the shifting and booth operation
-	carry_select_adder adder(adder_input, booth_output, input_DFF, carry_in, data_exception); //adds it and throws it back to the DFFE
+	carry_select_adder adder(adder_input, booth_output, input_DFF, carry_in, exception_wire[1]); //adds it and throws it back to the DFFE
 
 endmodule
