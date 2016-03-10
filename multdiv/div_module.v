01483c13d589912efcb63b39a3080bca7dc323fb
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-03-10 03:23:12 -0500

    added the mult and tested it is working; trigger work on div

--- multdiv/div_module.v
+++ multdiv/div_module.v
@@ -1,8 +1,11 @@
-module div_module(data_operandA, data_operandB, clock, temp_div_result, div_exception, div_inputRDY, div_resultRDY);
+module div_module(data_operandA, data_operandB, clock, data_result, data_exception, data_inputRDY, data_resultRDY);
    input [31:0] data_operandA;
    input [15:0] data_operandB;
    input clock;      
    output [31:0] data_result; 
-   output data_exception, data_inputRDY, data_resultRDY; 
+   output data_exception, data_inputRDY, data_resultRDY;
+	
+	
+	
 
 endmodule
--- multdiv/mult_module.v
+++ multdiv/mult_module.v
@@ -1,7 +1,7 @@
-module mult_module(data_operandA, data_operandB, clock, data_result, data_exception, data_inputRDY, data_resultRDY);
+module mult_module(data_operandA, data_operandB,ctrl_MULT, clock, data_result, data_exception, data_inputRDY, data_resultRDY);
    input [31:0] data_operandA;
    input [15:0] data_operandB;
-   input clock;      
+   input clock,ctrl_MULT;      
    output [31:0] data_result; 
    output data_exception, data_inputRDY, data_resultRDY; 
 	
@@ -41,8 +41,8 @@ module mult_module(data_operandA, data_operandB, clock, data_result, data_except
 	wire[2:0] counter_output;
 	wire in_signal; //the in_signal is used to reset the wire, effectively turning the counter to be a mod k counter where k <= 8
 	counter count_8(clock, in_signal, counter_output);
-	assign in_signal = counter_output[0] & counter_output[1] & counter_output[2]; //currently mod 8 counter; change here to change it
-	assign data_resultRDY = in_signal;
+	assign in_signal = ~ctrl_MULT; 
+	assign data_resultRDY = (counter_output[0] & counter_output[1] & counter_output[2]); //currently mod 8 counter; change here to change it
 	assign data_inputRDY = ~counter_output[0] & ~counter_output[1] & ~counter_output[2];
 	
 	/*
