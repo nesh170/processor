a85fc3fef10b87b72c499a0c791dea9a084c43ee
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-03-11 22:06:27 -0500

    made changes but does not work

--- multdiv/div_module.v
+++ multdiv/div_module.v
@@ -11,21 +11,49 @@ module div_module(data_operandA, data_operandB, clock, data_result, data_excepti
 	wire predicted_sign;
 	wire[31:0] dividend,divisor;
 	assign divisor[31:16] = 0;
-	sign_checker(data_operandA,data_operandB,dividend,divisor[15:0],predicted_sign);
+	sign_checker check_sign(data_operandA,data_operandB,dividend,divisor[15:0],predicted_sign); //tested and works
 	
 	assign data_exception = (~|divisor); //DIV by 0 Check
 	
 	
 	wire[31:0] aligned_divisor;
 	align_divider aligner(dividend,divisor,aligned_divisor);
+		
+	wire[31:0] temp_divisor[31:0];
+	wire[31:0] remainder[31:0];
+	wire[31:0] temp_quotient[31:0];
+	assign remainder[0] = dividend;
+	assign temp_divisor[0] = aligned_divisor;
+	assign temp_quotient[0] = 32'b0;
 	
-	//ALU(data_operandA, data_operandB, 00001, ctrl_shiftamt, data_result, isNotEqual, isLessThan);
+	/*
+	try to figure whether 1 or 0: if A<B, then choose 0
+	*/
+	// //A<B
+	// subtractor_32bit(in_A, in_B, out, carry_out)
 	
 	genvar index;
 	generate
 	for(index = 0; index<31; index=index+1) begin: divisor_loop
-		
+		wire isNotEqual,isLessThan,divisor_isNotEqual,divisor_isLessThan;
+		wire[31:0] temp_wire;
+		wire[31:0] temp_remainder; //because we need to pick between - 0 or not
+		ALU subtractor(remainder[index],temp_divisor[index], 5'b00001, 5'b0, temp_remainder, isNotEqual, isLessThan);
+		ALU subtractor_divisor(remainder[index],divisor, 5'b00001, 5'b0, temp_wire, divisor_isNotEqual, divisor_isLessThan); //figure out if shift or not
+		assign remainder[index+1] = (isLessThan) ? remainder[index] : temp_remainder; //is a<B then subtract 0 else subtract temp_divisor
+		wire[31:0] w1;
+		wire[31:0] temp_shift_wire;
+		assign temp_shift_wire = (temp_quotient[index] << 1);
+		assign w1[31:1] = temp_shift_wire[31:1];
+		assign w1[0] = (divisor_isLessThan) ? temp_shift_wire[0] : ~isLessThan;
+		assign temp_quotient[index+1] = (divisor_isLessThan) ? temp_quotient[index] : w1[31:0];
 	end
 	endgenerate
+	
+	wire[31:0] positive_temp_quotient,negative_temp_quotient;
+	assign positive_temp_quotient = temp_quotient[31];
+	carry_select_adder adder(~positive_temp_quotient, 0, negative_temp_quotient, 1);
+	//assign data_result = (predicted_sign) ? negative_temp_quotient : positive_temp_quotient;
+	assign data_result = temp_quotient[31];
 
 endmodule
--- multdiv/sign_checker.v
+++ multdiv/sign_checker.v
@@ -10,19 +10,19 @@ module sign_checker(data_operandA,data_operandB,output_A,output_B,output_sign);
 	/*
 	This modules checks for the sign if they are negative and predicts the output sign
 	*/
-	assign output_sign = output_A[31]^output_B[15]; //figures out what the value is at the end
+	assign output_sign = data_operandA[31]^data_operandB[15]; //figures out what the value is at the end
 	
 	wire[31:0] temp_A_flip;
 	wire[15:0] temp_B_flip;
 	
-	carry_select_adder flip_A(~output_A, 0, temp_A_flip, 1); //flip the sign
-	carry_select_adder flip_B(~output_B, 0, temp_B_flip, 1); //flip the sign
+	carry_select_adder flip_A(~data_operandA, 0, temp_A_flip, 1); //flip the sign
+	carry_select_adder flip_B(~data_operandB, 0, temp_B_flip, 1); //flip the sign
 	
 	
-	assign output_A = (~output_A[31]) ? data_operandA :32'bZ;
-	assign output_A = (output_A[31]) ? temp_A_flip :32'bZ; //the original is negative, make it +ve
+	assign output_A = (~data_operandA[31]) ? data_operandA :32'bZ;
+	assign output_A = (data_operandA[31]) ? temp_A_flip :32'bZ; //the original is negative, make it +ve
 	
-	assign output_B = (~output_B[15]) ? data_operandB : 16'bZ;
-	assign output_B = (output_B[15]) ? temp_B_flip : 16'bZ; //the original is negative, make it +ve
+	assign output_B = (~data_operandB[15]) ? data_operandB : 16'bZ;
+	assign output_B = (data_operandB[15]) ? temp_B_flip : 16'bZ; //the original is negative, make it +ve
 	
 endmodule
