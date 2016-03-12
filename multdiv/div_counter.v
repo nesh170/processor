ef368c20606d3ac53d703fe501dc574a91643d11
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-03-12 12:48:29 -0500

    added the counter, lets test it

--- multdiv/div_counter.v
+++ multdiv/div_counter.v
@@ -1,44 +1,40 @@
 module div_counter(clock, in_signal, out, present, next);
 	input clock, in_signal;
-	output[6:0] out;
-	output [6:0] present,next;
+	output[2:0] out;
+	output [5:0] present,next;
 	
 	
-	wire [6:0] present;
+	wire [5:0] present;
 	
 	reg[2:0] out;
-	reg[6:0] next;
+	reg[5:0] next;
 	
 	always @(*) begin
-		case(present)
-			`state0: next = in_signal ? `state0 : `state1;
-			`state1: next = in_signal ? `state0 : `state2;
-			`state2: next = in_signal ? `state0 : `state3;
-			`state3: next = in_signal ? `state0 : `state4;
-			`state4: next = in_signal ? `state0 : `state5;
-			`state5: next = in_signal ? `state0 : `state6;
-			`state6: next = in_signal ? `state0 : `state7;
-			`state7: next = in_signal ? `state0 : `state0;
-			default: next = `state0;
-		endcase
+		if(in_signal)
+			next = 6'b0;
+		else if(present~=6'b100001)
+			next = present + 1;
+		else if(present == 6'b100001)
+			next = 6'b0;
+		else 
+			next = 6'b0;
+		end
 	end
 	
 	always @(present) begin
-		case(present)
-			`state0: out = `state0;
-			`state1: out = `state1;
-			`state2: out = `state2;
-			`state3: out = `state3;
-			`state4: out = `state4;
-			`state5: out = `state5;
-			`state6: out = `state6;
-			`state7: out = `state7;
-			default: out = `state0;
-		endcase
+		if(present==6'b0)
+			out = 2'b01;
+		else if(present == 6'b100001)
+			out = 2'b10;
+		else 
+			out = 2'b00;
+		end
 	end
 
 	DFF state_reg_0(.clk(clock), .d(next[0]), .q(present[0]));
 	DFF state_reg_1(.clk(clock), .d(next[1]), .q(present[1]));
 	DFF state_reg_2(.clk(clock), .d(next[2]), .q(present[2]));
-	
+	DFF state_reg_3(.clk(clock), .d(next[3]), .q(present[3]));
+	DFF state_reg_4(.clk(clock), .d(next[4]), .q(present[4]));
+	DFF state_reg_5(.clk(clock), .d(next[5]), .q(present[5]));
 endmodule
