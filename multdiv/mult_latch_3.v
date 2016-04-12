7dc7560158688ff366d4fb8802278f265ae88f8d
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-04-12 13:49:20 -0400

    reverted back to 8f18c40305e00c06a95266a815013c81018064b6

--- multdiv/mult_latch_3.v
+++ multdiv/mult_latch_3.v
@@ -1,7 +1,7 @@
-module mult_latch_3(input_0,input_1,input_2,input_3,output_0,output_1,output_2,output_3,clock,reg_input,reg_output,ins_input,ins_output);
-	input[31:0] input_0,input_1,input_2,input_3,reg_input,ins_input;
+module mult_latch_3(input_0,input_1,input_2,input_3,output_0,output_1,output_2,output_3,clock,reg_input,reg_output);
+	input[31:0] input_0,input_1,input_2,input_3,reg_input;
 	input clock;
-	output[31:0] output_0,output_1,output_2,output_3,reg_output,ins_output;
+	output[31:0] output_0,output_1,output_2,output_3,reg_output;
 
 
 	register reg_0(.bitsIn(input_0), .bitsOut(output_0), .writeEnable(1'b1), .reset(1'b0), .clk(clock));
@@ -10,5 +10,4 @@ module mult_latch_3(input_0,input_1,input_2,input_3,output_0,output_1,output_2,o
 	register reg_3(.bitsIn(input_3), .bitsOut(output_3), .writeEnable(1'b1), .reset(1'b0), .clk(clock));
 	register reg_extra(.bitsIn(reg_input), .bitsOut(reg_output), .writeEnable(1'b1), .reset(1'b0), .clk(clock));
 	
-	register reg_instruction(.bitsIn(ins_input), .bitsOut(ins_output), .writeEnable(1'b1), .reset(1'b0), .clk(clock));
 endmodule
--- multdiv/mult_module.v
+++ multdiv/mult_module.v
@@ -1,9 +1,9 @@
-module mult_module(data_A, data_B,mult_signal, clock, data_result, exception, input_RDY, result_RDY,input_ins,instruction_1,instruction_2,instruction_3,instruction_4);
-	input[31:0] data_A,input_ins;
+module mult_module(data_A, data_B,mult_signal, clock, data_result, exception, input_RDY, result_RDY);
+	input[31:0] data_A;
 	input[15:0] data_B;
 	input mult_signal,clock;
 
-	output[31:0] data_result,instruction_1,instruction_2,instruction_3,instruction_4;
+	output[31:0] data_result;
 	output exception,input_RDY,result_RDY;
 	
 	//Operations before LATCH 1
@@ -38,15 +38,15 @@ module mult_module(data_A, data_B,mult_signal, clock, data_result, exception, in
 	genvar index;
 	generate
 		for(index=0; index<8; index = index + 1) begin: loop_structure
-			booth_decoder decoder(.shifted_multiplicand((multiplicand << (2*index))),.booth_operation(booth_input[index]),.output_multiplicand(booth_output[index]));
+			booth_decoder decoder(.shifted_multiplicand((data_A << (2*index))),.booth_operation(booth_input[index]),.output_multiplicand(booth_output[index]));
 		end
 	endgenerate
 	
 	
 	//LATCH 1
-	wire[31:0] latch_1_important,latch_1_output_instruction;
+	wire[31:0] latch_1_important;
 	wire[31:0] latch_1_output[7:0];
-	mult_latch_2 latch_1(.input_0(booth_output[0]),.input_1(booth_output[1]),.input_2(booth_output[2]),.input_3(booth_output[3]),.input_4(booth_output[4]),.input_5(booth_output[5]),.input_6(booth_output[6]),.input_7(booth_output[7]),.output_0(latch_1_output[0]),.output_1(latch_1_output[1]),.output_2(latch_1_output[2]),.output_3(latch_1_output[3]),.output_4(latch_1_output[4]),.output_5(latch_1_output[5]),.output_6(latch_1_output[6]),.output_7(latch_1_output[7]),.clock(clock),.reg_input(important_bits),.reg_output(latch_1_important),.ins_input(input_ins),.ins_output(latch_1_output_instruction));
+	mult_latch_2 latch_1(.input_0(booth_output[0]),.input_1(booth_output[1]),.input_2(booth_output[2]),.input_3(booth_output[3]),.input_4(booth_output[4]),.input_5(booth_output[5]),.input_6(booth_output[6]),.input_7(booth_output[7]),.output_0(latch_1_output[0]),.output_1(latch_1_output[1]),.output_2(latch_1_output[2]),.output_3(latch_1_output[3]),.output_4(latch_1_output[4]),.output_5(latch_1_output[5]),.output_6(latch_1_output[6]),.output_7(latch_1_output[7]),.clock(clock),.reg_input(important_bits),.reg_output(latch_1_important));
 	
 	//Stage 1 operations, adding the a tree structure 0+1,2+3,4+5,6+7;
 	wire[31:0] adder_1_output[3:0];
@@ -56,9 +56,9 @@ module mult_module(data_A, data_B,mult_signal, clock, data_result, exception, in
 	carry_select_adder add_stage_1_4(.in_A(latch_1_output[6]), .in_B(latch_1_output[7]), .out(adder_1_output[3]), .carry_in(1'b0));
 	
 	//LATCH 2
-	wire[31:0] latch_2_important,latch_2_output_instruction;
+	wire[31:0] latch_2_important;
 	wire[31:0] latch_2_output[3:0];
-	mult_latch_3 latch_2(.input_0(adder_1_output[0]),.input_1(adder_1_output[1]),.input_2(adder_1_output[2]),.input_3(adder_1_output[3]),.output_0(latch_2_output[0]),.output_1(latch_2_output[1]),.output_2(latch_2_output[2]),.output_3(latch_2_output[3]),.clock(clock),.reg_input(latch_1_important),.reg_output(latch_2_important),.ins_input(latch_1_output_instruction),.ins_output(latch_2_output_instruction));
+	mult_latch_3 latch_2(.input_0(adder_1_output[0]),.input_1(adder_1_output[1]),.input_2(adder_1_output[2]),.input_3(adder_1_output[3]),.output_0(latch_2_output[0]),.output_1(latch_2_output[1]),.output_2(latch_2_output[2]),.output_3(latch_2_output[3]),.clock(clock),.reg_input(latch_1_important),.reg_output(latch_2_important));
 	
 	//Stage 2 operations, adding in a tree structure (0+1) + (2+3), (4+5) + (6+7)
 	wire[31:0] adder_2_output[1:0];
@@ -66,9 +66,9 @@ module mult_module(data_A, data_B,mult_signal, clock, data_result, exception, in
 	carry_select_adder add_stage_3_2(.in_A(latch_2_output[2]), .in_B(latch_2_output[3]), .out(adder_2_output[1]), .carry_in(1'b0));
 	
 	//LATCH 3
-	wire[31:0] latch_3_important,latch_3_output_instruction;
+	wire[31:0] latch_3_important;
 	wire[31:0] latch_3_output[1:0];
-	mult_latch_1_4 latch_3(.input_A(adder_2_output[0]),.input_B(adder_2_output[1]),.output_A(latch_3_output[0]),.output_B(latch_3_output[1]),.clock(clock),.reg_input(latch_2_important),.reg_output(latch_3_important),.ins_input(latch_2_output_instruction),.ins_output(latch_3_output_instruction));
+	mult_latch_1_4 latch_3(.input_A(adder_2_output[0]),.input_B(adder_2_output[1]),.output_A(latch_3_output[0]),.output_B(latch_3_output[1]),.clock(clock),.reg_input(latch_2_important),.reg_output(latch_3_important));
 
 	//Stage 3 operations, adding in a tree structure ((0+1) + (2+3)) + ((4+5) + (6+7))
 	wire[31:0] adder_3_output;
@@ -78,17 +78,11 @@ module mult_module(data_A, data_B,mult_signal, clock, data_result, exception, in
 	assign important_bits_3[1] = latch_3_important[1];
 
 	//FINAL Latch
-	wire[31:0] final_important_bits,latch_4_output_instruction;
-	mult_latch_1_4 latch_4(.input_A(adder_3_output),.reg_input(important_bits_3),.output_A(data_result),.clock(clock),.reg_output(final_important_bits),.ins_input(latch_3_output_instruction),.ins_output(latch_4_output_instruction));
+	wire[31:0] final_important_bits;
+	mult_latch_1_4(.input_A(adder_3_output),.reg_input(important_bits_3),.output_A(data_result),.clock(clock),.reg_output(final_important_bits));
 	assign exception = final_important_bits[0];
 	assign result_RDY = final_important_bits[1];
 
 	assign input_RDY =1'b1; //always ready to acccept input since it opens a new space every clock cycle
-
-	//Assigning all the output port instructions
-	assign instruction_1 = latch_1_output_instruction;
-	assign instruction_2 = latch_2_output_instruction;
-	assign instruction_3 = latch_3_output_instruction;
-	assign instruction_4 = latch_4_output_instruction;
 	
 endmodule
