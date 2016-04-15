35e879afc9a999d28dbba1408783620a8474162f
Ankit Kayastha <ankit.kayastha@duke.edu>
2016-04-15 12:14:03 -0400

    pls work

--- assembly_code/white_screen_pls/screen.asm
+++ assembly_code/white_screen_pls/screen.asm
@@ -1,12 +1,20 @@
 .text
 main:
 addi $r2, $r0, 0
-addi $r3, $r0, 3071990
+addi $r3, $r0, 307199
 addi $r1, $r0, 255
 j loop
 loop:
 bgt $r2, $r3, exit
+nop
+nop
+nop
+nop
 sw $r1, 0($r2)
+nop
+nop
+nop
+nop
 addi $r2, $r2, 1
 j loop
 exit:
