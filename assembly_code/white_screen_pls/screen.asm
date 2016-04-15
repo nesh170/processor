24c45e7533e2a5e15b3b1cad3173985c0bafc084
Ankit Kayastha <ankit.kayastha@duke.edu>
2016-04-15 16:05:12 -0400

    should cycle through colors

--- assembly_code/white_screen_pls/screen.asm
+++ assembly_code/white_screen_pls/screen.asm
@@ -1,20 +1,23 @@
 .text
 main:
 addi $r2, $r0, 0
-addi $r3, $r3, 60000
-addi $r3, $r3, 60000
-addi $r3, $r3, 60000
-addi $r3, $r3, 60000
-addi $r3, $r3, 60000
-addi $r3, $r3, 7200
-addi $r1, $r0, 255
-j loop
+lw $r3, 0($r0)
+addi $r1, $r0, 0
+addi $r4, $r0, 255
+j color_loop
+color_loop:
+bgt $r1, $r4, exit_color
+jal loop
+addi $r1, $r1, 1
+j color_loop
 loop:
 bgt $r2, $r3, exit
 sw $r1, 0($r2)
 addi $r2, $r2, 1
 j loop
 exit:
+jr $ra
+exit_color:
 halt
 .data
 addr: .word 0x0004AFFF
