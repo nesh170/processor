4d89d77397600f35bfefa05a6abf4fb522b0b326
Ankit Kayastha <ankit.kayastha@duke.edu>
2016-04-16 01:51:16 -0400

    pls work

--- assembly_code/final.asm
+++ assembly_code/final.asm
@@ -13,11 +13,11 @@ main:
 # register 17 holds the indexing for the for loop
 # register 18 holds the color that is loaded from data memory
 # register 19 holds the coordinate value limit for drawing
-lw $r4, left($r0)
-lw $r5, right($r0)
-lw $r8, a($r0)
-lw $r9, s($r0)
-lw $r10, d($r0)
+lw $r4, 0($r0)
+lw $r5, 1($r0)
+lw $r8, 2($r0)
+lw $r9, 3($r0)
+lw $r10, 4($r0)
 addi $r14, $r0, 160
 addi $r15, $r0, 320
 addi $r16, $r0, 480
@@ -27,19 +27,19 @@ addi $r7, $r7,-120 #increment player's position for time - UPDATE CALL TO REFLEC
 jal check_player_pos
 addi $r1, $r1, 1 #register 1 holds time
 jal check_time
-beq $r6, $r4, move_left #z pressed
-beq $r6, $r5, move_right #x pressed
+bne $r6, $r4, move_left #z pressed
+bne $r6, $r5, move_right #x pressed
 continue_game_loop:
-beq $r6, $r8, a_press 
-beq $r6, $r9, s_press
-beq $r6, $r10, d_press
+bne $r6, $r8, a_press 
+bne $r6, $r9, s_press
+bne $r6, $r10, d_press
 check_bird_bug: #check for bug intersecting bird
-beq $r7, $r11, quit
-beq $r7, $r12, quit
-beq $r7, $r13, quit
+bne $r7, $r11, quit
+bne $r7, $r12, quit
+bne $r7, $r13, quit
 # render screen, draw right line first
 addi $r17, $r0, 307040
-lw $r18, color_line($r0)
+lw $r18, 5($r0)
 addi $r19, $r0, 480
 jal draw_line
 addi $r17, $r0, 306880
@@ -50,16 +50,17 @@ jal draw_line
 addi $r17, $r0, 304160
 addi $r19, $r0, 160
 jal draw_line
-lw $r18, color_line($r0)
-jal draw_bug
-jal draw_bird
+lw $r18, 6($r0)
+#jal draw_bug
+lw $r18, 7($r0)
+#jal draw_bird
 # jump back to game loop
 j game_loop
 # if player position less than (160, 320, 480) mod back to bottom of screen
 check_player_pos:
-beq $r7, $r14, mod_left
-beq $r7, $r15, mod_center
-beq $r7, $r16, mod_right
+bne $r7, $r14, mod_left
+bne $r7, $r15, mod_center
+bne $r7, $r16, mod_right
 jr $r31
 mod_left:
 addi $r7, $r0, 306720
@@ -72,7 +73,7 @@ addi $r7, $r0, 307040
 jr $r31
 check_time:
 addi $r2, $r0, 50000 #add 50000 to register 2 - 50000 is clock freq
-beq $r1, $r2, update_time
+bne $r1, $r2, update_time
 # r1 = 50000, increment r2 that will be displayed on 7 seg display
 jr $r31
 update_time:
@@ -95,24 +96,24 @@ d_press:
 addi $r13, $r7, 100 # CHANGE THIS CALL
 j check_bird_bug
 draw_line:
-swd r18, 0($r17)
+sw $r18, 0($r17)
 addi $r17, $r17, -640
 blt $r17, $r19, stop_draw_line
 j draw_line
 stop_draw_line:
 jr $ra
-draw_bug:
-stop_draw_bug:
-jr $ra
+#draw_bug:
+#stop_draw_bug:
+#jr $ra
 quit:
 addi $r30, $r0, -1
 halt
 .data
-left: .char z
-right: .char x
+left: .word 0x0000006B
+right: .word 0x00000074
 a: .char a
 s: .char s
 d: .char d
-color_line .word 0x00ffffff
-color_bug .word 0x00ffffff
-color_bird .word 0x00ffffff
+color_line: .word 0x00ffffff
+color_bug: .word 0x00ffffff
+color_bird: .word 0x00ffffff
--- assembly_code/white_screen_pls/screen.asm
+++ assembly_code/white_screen_pls/screen.asm
@@ -2,22 +2,14 @@
 main:
 addi $r2, $r0, 0
 lw $r3, 0($r0)
-addi $r1, $r0, 0
 addi $r4, $r0, 255
-j color_loop
-color_loop:
-bgt $r1, $r4, exit_color
-jal loop
-addi $r1, $r1, 1
-j color_loop
+j loop
 loop:
 bgt $r2, $r3, exit
-sw $r1, 0($r2)
+sw $r4, 0($r2)
 addi $r2, $r2, 1
 j loop
 exit:
-jr $ra
-exit_color:
 halt
 .data
 addr: .word 0x0004AFFF
