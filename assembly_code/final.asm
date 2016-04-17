da69d4ee01fb59cd4e312e9a7badc332170658d8
Ankit Kayastha <ankit.kayastha@duke.edu>
2016-04-17 04:41:34 -0400

    making gainz hopefully

--- assembly_code/final.asm
+++ assembly_code/final.asm
@@ -13,20 +13,39 @@ main:
 # register 17 holds the indexing for the for loop
 # register 18 holds the color that is loaded from data memory
 # register 19 holds the coordinate value limit for drawing
+# register 20 holds the ending address for background color for testing (black)
+# register 21 holds the index for the for loop
+# register 22 holds the color to store (red)
+# register 23 holds the offset for drawing bounding box (50)
+# register 24 holds the offset for drawing other offset of bounding box (-50)
 lw $r4, 0($r0)
 lw $r5, 1($r0)
+lw $r7, 12($r0) #upload initial position
 lw $r8, 2($r0)
 lw $r9, 3($r0)
 lw $r10, 4($r0)
 addi $r14, $r0, 160
 addi $r15, $r0, 320
 addi $r16, $r0, 480
+# ending address can't be expressed in 17 bits for addi, use lw
+lw $r20, 8($r0)
+addi $r21, $r21, 0
+addi $r22, $r0, 92
+j background_loop
+background_loop:
+bgt $r21, $r20, exit_background
+sw $r22, 0($r21)
+addi $r21, $r21, 1
+j background_loop
+exit_background:
 j game_loop
 game_loop:
-addi $r7, $r7,-120 #increment player's position for time - UPDATE CALL TO REFLECT CORRECT VALUE
-jal check_player_pos
+#addi $r7, $r7,-640 #increment player's position for time - UPDATE CALL TO REFLECT CORRECT VALUE
+#jal check_player_pos
 addi $r1, $r1, 1 #register 1 holds time
 jal check_time
+jal check_player_pos
+addi $r6, $r0, 0
 bne $r6, $r4, move_left #z pressed
 bne $r6, $r5, move_right #x pressed
 continue_game_loop:
@@ -38,20 +57,33 @@ bne $r7, $r11, quit
 bne $r7, $r12, quit
 bne $r7, $r13, quit
 # render screen, draw right line first
-addi $r17, $r0, 307040
+lw $r18, 13($r0)
+lw $r17, 9($r0)
+addi $r19, $r0, 480
+# draws right bounding box
+jal draw_bounding_box
+# draw middle bounding box
+lw $r17, 10($r0)
+addi $r19, $r0, 320
+jal draw_bounding_box
+# draw left bounding box
+lw $r17, 11($r0)
+addi $r19, $r0, 160
+jal draw_bounding_box
+lw $r17, 9($r0)
 lw $r18, 5($r0)
 addi $r19, $r0, 480
 jal draw_line
-addi $r17, $r0, 306880
+lw $r17, 10($r0)
 # register18 should still have the color of the line, draw middle line
 addi $r19, $r0, 320
 jal draw_line
 # draw left line
-addi $r17, $r0, 304160
+lw $r17, 11($r0)
 addi $r19, $r0, 160
 jal draw_line
 lw $r18, 6($r0)
-#jal draw_bug
+jal draw_bug
 lw $r18, 7($r0)
 #jal draw_bird
 # jump back to game loop
@@ -63,21 +95,22 @@ bne $r7, $r15, mod_center
 bne $r7, $r16, mod_right
 jr $r31
 mod_left:
-addi $r7, $r0, 306720
+lw $r7, 11($r0)
 jr $r31
 mod_center:
-addi $r7, $r0, 306880
+lw $r7, 10($r0)
 jr $r31
 mod_right:
-addi $r7, $r0, 307040
+lw $r7, 9($r0)
 jr $r31
 check_time:
-addi $r2, $r0, 50000 #add 50000 to register 2 - 50000 is clock freq
+addi $r2, $r0, 5000 #add 50000 to register 2 - 50000 is clock freq
 bne $r1, $r2, update_time
 # r1 = 50000, increment r2 that will be displayed on 7 seg display
 jr $r31
 update_time:
 addi $r3, $r3, 1
+addi $r7, $r7, -640
 add $r1, $r0, $r0
 jr $r31
 move_left:
@@ -102,9 +135,38 @@ blt $r17, $r19, stop_draw_line
 j draw_line
 stop_draw_line:
 jr $ra
-#draw_bug:
+draw_bug: # contains position of bug
+sw $r18, 0($r7)
+sw $r18, -640($r7)
+sw $r18, -1280($r7)
+sw $r18, 1($r7)
+sw $r18, -1($r7)
+sw $r18, 2($r7)
+sw $r18, -2($r7)
+sw $r18, 640($r7)
+sw $r18, 641($r7)
+sw $r18, 642($r7)
+sw $r18, 639($r7)
+sw $r18, 638($r7)
 #stop_draw_bug:
-#jr $ra
+jr $ra
+draw_bounding_box:
+add $r30, $r31, $r0 # move previous ra to $r30
+addi $r23, $r17, -50
+addi $r24, $r17, 50
+jal actual_draw
+blt $r17, $r19, stop_drawing
+addi $r17, $r17, -640
+j draw_bounding_box
+stop_drawing:
+jr $r30
+actual_draw:
+bgt $r23, $r24, exit_actual_draw
+sw $r18, 0($r23)
+addi $r23, $r23, 1
+j actual_draw
+exit_actual_draw:
+jr $ra
 quit:
 addi $r30, $r0, -1
 halt
@@ -114,6 +176,12 @@ right: .word 0x00000074
 a: .char a
 s: .char s
 d: .char d
-color_line: .word 0x00ffffff
-color_bug: .word 0x00ffffff
+color_line: .word 0x000000ff
+color_bug: .word 0x0000000C
 color_bird: .word 0x00ffffff
+color_background: .word 0x0004AFFF
+right_line_limit: .word 0x0004AF60
+middle_line_limit: .word 0x0004AEC0
+left_line_limit: .word 0x0004A420
+initial_position: .word 0x0004A4C0
+color_bounding_box: .word 0x00000000
