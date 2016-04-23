885fbb6146c9615dd5b8c95c3e1604a855af5976
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-04-22 23:31:00 -0400

    figured out need to add 2 noop between lw ad branch only

---
+++ README.md
@@ -0,0 +1,10 @@
+README
+======
+
+Processor
+
+Ankit Kayastha
+Sivaneshwaran Loganathan
+
+Bugs:
+Need to add 2 noop in between a lw and branch due to stall logic with Fast Branch
--- assembly_code/lw_test/test.asm
+++ assembly_code/lw_test/test.asm
@@ -1,22 +1,32 @@
 .text
 main:
-addi $r6, $r0, 2
+addi $r6, $r0, 1
 lw $r4, 1($r0)
 nop
 nop
-beq $r4, $r6, move_left #z pressed
+bne $r4, $r6, move_left #z pressed
+round:
 lw $r4, 0($r0)
 nop
 nop
-beq $r4, $r6, move_right #x pressed
+bne $r4, $r6, move_right #x pressed
+sky:
+lw $r12, 2($r0)
+addi $r18,$r12,52
 move_left:
 addi $r8,$r7,5
-addi $r6,$r0,1
-j exit
+addi $r6,$r0,2
+j round
 move_right:
 addi $r8,$r7,20
+j sky
+
+
+
+j exit
 exit:
 halt
 .data
 left: .word 0x00000001
 right: .word 0x00000002
+temp: .word 0x00000003
